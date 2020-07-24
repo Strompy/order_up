f0eeb4d57639ea5068025cc91281691948bb6938
Phillip Strom <phillip.strom@gmail.com>
2020-07-24 09:41:38 -0600

    Create User Story 2 test

--- spec/features/dish/show_spec.rb
+++ spec/features/dish/show_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe "Dish Show Page" do
-  it "shows a list of ingredients and the chef's name" do
+  before :each do
     @chef = Chef.create!(name: "Boy R Dee")
     @dish1 = @chef.dishes.create!(name: "Ravioli", description: "Like a an explosion of flavor")
     @ingredient1 = Ingredient.create!(name: "Pasta", calories: 1_000)
@@ -10,18 +10,20 @@ RSpec.describe "Dish Show Page" do
     DishIngredient.create!(ingredient: @ingredient1, dish: @dish1)
     DishIngredient.create!(ingredient: @ingredient2, dish: @dish1)
     DishIngredient.create!(ingredient: @ingredient3, dish: @dish1)
+  end
 
+  it "shows a list of ingredients and the chef's name" do
     visit "/dish/#{@dish1.id}"
-    
+
     expect(page).to have_content(@ingredient1.name)
     expect(page).to have_content(@ingredient2.name)
     expect(page).to have_content(@ingredient3.name)
     expect(page).to have_content(@chef.name)
   end
-end
 
-# Story 1 of 3
-# As a visitor
-# When I visit a dish's show page
-# I see a list of ingredients for that dish
-# and the chef's name
+  it "displays the total calorie count" do
+    visit "/dish/#{@dish1.id}"
+
+    expect(page).to have_content("Calories: 1,510")
+  end
+end
