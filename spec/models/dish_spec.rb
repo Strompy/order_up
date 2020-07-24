d1808b16ebd5233fc4a88c367687f7311c10a3c6
Phillip Strom <phillip.strom@gmail.com>
2020-07-24 09:45:53 -0600

    Create total_calories method

--- spec/models/dish_spec.rb
+++ spec/models/dish_spec.rb
@@ -10,4 +10,19 @@ RSpec.describe Dish, type: :model do
     it {should have_many :dish_ingredients}
     it {should have_many(:ingredients).through(:dish_ingredients)}
   end
+  describe "instance methods" do
+    before :each do
+      @chef = Chef.create!(name: "Boy R Dee")
+      @dish1 = @chef.dishes.create!(name: "Ravioli", description: "Like a an explosion of flavor")
+      @ingredient1 = Ingredient.create!(name: "Pasta", calories: 1_000)
+      @ingredient2 = Ingredient.create!(name: "Cheese", calories: 500)
+      @ingredient3 = Ingredient.create!(name: "Sauce", calories: 10)
+      DishIngredient.create!(ingredient: @ingredient1, dish: @dish1)
+      DishIngredient.create!(ingredient: @ingredient2, dish: @dish1)
+      DishIngredient.create!(ingredient: @ingredient3, dish: @dish1)
+    end
+    it "#total_calories" do
+      expect(@dish1.total_calories).to eq(1_510)
+    end
+  end
 end
